6dea08dd209feed0cd6cc22875b34871576cf6aa
faizsarwar44 <faizsarwar44@gmail.com>
2023-07-20 01:36:06 +0500

    added code

---
+++ myproject/faq_blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class FAQ(models.Model):
+    question = models.CharField(max_length=255)
+    answer = models.TextField()
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+
+class Review(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    blog = models.ForeignKey(Blog, related_name='reviews', on_delete=models.CASCADE)
+
+class ContactUs(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
